7236d7516da6586080912f892fc9ad2aa29def13
Hadi Sabounchi <hadisabounchi@gmail.com>
2026-07-13 14:20:31 +0330

    finish refactor code and Adding player interaction future.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const players = document.querySelectorAll('.player');
+const currentScoreEl = document.querySelectorAll('.current-score');
+const playersNameEl = document.querySelectorAll('.name');
 
 const diceEl = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn--roll');
@@ -11,21 +13,40 @@ const newBtn = document.querySelector('.btn--new');
 const holdBtn = document.querySelector('.btn--hold');
 
 // initial condition
-let scores, activePlayer, currentScore;
+let scores,
+  activePlayer,
+  currentScore,
+  PlayersName = [],
+  finishPoint;
 
 const gamePresets = function () {
   scores = [0, 0];
   activePlayer = 0;
   currentScore = 0;
+  currentScoreEl[0].textContent = 0;
+  currentScoreEl[1].textContent = 0;
   score0El.textContent = 0;
   score1El.textContent = 0;
   diceEl.classList.add('hidden');
   rollBtn.classList.remove('hidden');
   holdBtn.classList.remove('hidden');
   players[activePlayer].classList.add('player--active');
-};
 
-gamePresets();
+  PlayersName[0] = prompt('what is the name of first player ?');
+  PlayersName[1] = prompt('what is the name of second player ?');
+  finishPoint = Number(prompt('Please enter the finishing score:'));
+
+  finishPoint && finishPoint > 0 && !isNaN(finishPoint)
+    ? finishPoint
+    : (finishPoint = 100);
+
+  PlayersName[0]
+    ? (playersNameEl[0].textContent = PlayersName[0])
+    : (playersNameEl[0].textContent = 'PLAYER 1');
+  PlayersName[1]
+    ? (playersNameEl[1].textContent = PlayersName[1])
+    : (playersNameEl[1].textContent = 'PLAYER 2');
+};
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -39,10 +60,11 @@ const switchPlayer = function () {
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
 };
 
+gamePresets();
+
 // ------  Dice rolling FN  ------
 
 rollBtn.addEventListener('click', () => {
-  console.log(activePlayer);
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
 
   diceEl.classList.remove('hidden');
@@ -70,7 +92,7 @@ holdBtn.addEventListener('click', () => {
     scores[activePlayer];
 
   // check for possible winner
-  if (scores[activePlayer] >= 30) {
+  if (scores[activePlayer] >= finishPoint) {
     document.querySelector(`#name--${activePlayer}`).textContent = 'WINNER !';
     players[activePlayer].classList.add('player--winner');
     players[activePlayer].classList.remove('player--active');
@@ -87,7 +109,5 @@ holdBtn.addEventListener('click', () => {
 
 newBtn.addEventListener('click', () => {
   players[activePlayer].classList.remove('player--winner');
-  document.querySelector(`#name--${activePlayer}`).textContent =
-    `PLAYER ${activePlayer + 1}`;
   gamePresets();
 });
